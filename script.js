a8c4de258025bf51854771ea13b9f07fce8f165e
sairi-hernandez-18 <sairihernandez81@gmail.com>
2026-04-13 14:45:12 -0600

    e

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function mostrarmensaje () {
         mensaje.innerHTML="🐾cuida y protege a los animales";
         mensaje.style.display="block";
 
-        setTimeout(l)=>{
+        setTimeout()=>{
             mensaje.style.opacity="1";
             mensaje.style.transform="translateY(0)";
         };50);
