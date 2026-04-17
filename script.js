0ca45300b70fb010232623a3d9cd0ea2b67ae749
sairi-hernandez-18 <sairihernandez81@gmail.com>
2026-04-17 09:55:07 -0600

    s

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ function mostrarMensaje() {
 function mostrarMensaje() {
     let mensaje = document.getElementById("mensaje");
 
-        mensaje.innerHTML = "🐾 si tines mascostas tu dever es cuidar de ellos";
+        mensaje.innerHTML = "🐾 si tienes mascostas tu dever es cuidar de ellos";
         mensaje.style.display = "block";
 
         setTimeout(() => {
