b5d394ba40fb7e0b52deb92d0d2afe9f3920fb75
sairi-hernandez-18 <sairihernandez81@gmail.com>
2026-04-17 09:44:55 -0600

    h

--- script.js
+++ script.js
@@ -10,3 +10,16 @@ function mostrarMensaje() {
         }, 50);
 
 }
+
+function mostrarMensaje() {
+    let mensaje = document.getElementById("mensaje");
+
+        mensaje.innerHTML = "🐾 si tines masc ostas tu devr es cuidar de el";
+        mensaje.style.display = "block";
+
+        setTimeout(() => {
+            mensaje.style.opacity= "1";
+            mensaje.style.transform = "translateY(0)";
+        }, 50);
+
+}
